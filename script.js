89f329a05e162f6120fd6a4e9dbd6d51171ec151
Joaoaugustohm <142924955+Joaoaugustohm@users.noreply.github.com>
2024-05-15 09:29:41 -0300

    add select + script atualizado

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 function calcularOrcamento() {
   // Obtendo os valores dos campos do formulário
-  var tamanhoMastro = parseFloat(document.getElementById("tamanhoMastro").value);
-  var quantidadeMastro = parseInt(document.getElementById("quantidadeMastro").value);
+  var tamanhoMastro1 = parseFloat(document.getElementById("tam1").value);
+  var tamanhoMastro2 = parseFloat(document.getElementById("tam2").value);
+  var quantidadeMastro1 = parseInt(document.getElementById("quantidadeMastro1").value);
+  var quantidadeMastro2 = parseInt(document.getElementById("quantidadeMastro2").value);
   var metrosQuadrados = parseFloat(document.getElementById("metrosQuadrados").value);
-  var ponteiras = parseInt(document.getElementById("ponteiras").value);
+  var bases = parseInt(document.getElementById("bases").value);
   var margemlucro = parseFloat(document.getElementById("margemlucro").value);
 
   // Defina os preços para cada item
@@ -13,24 +15,29 @@ function calcularOrcamento() {
   var precoPonteira = 75; // Preço por ponteira
   var precobase = 190; // Preço por base
   var homemhora = 40; // Preço por hora de mão de obra
+  var quantidadeponteira = 0;
 
   // Calculando o total para cada item
-  var totalMastro = tamanhoMastro * precoMastro * quantidadeMastro;
+  var quantidadeMastro = quantidadeMastro1 + quantidadeMastro2;
+  var tamanhoMastro = (tamanhoMastro1 * quantidadeMastro1) + (tamanhoMastro2 * quantidadeMastro2);
+  var totalMastro = (tamanhoMastro * precoMastro);
+  var totalponteira = (quantidadeMastro + quantidadeponteira) * precoPonteira;
   var totalLona = metrosQuadrados * precoLona;
-  var totalbaseponteira = ponteiras * (precoPonteira + precobase);
+  var totalbase = bases * precobase;
   var totalhora = (metrosQuadrados * 0.8) * homemhora;
   var margemlucro = (margemlucro / 100) + 1;
 
   // Calculando o total geral
-  var comp = ((totalMastro + totalLona + totalbaseponteira + totalhora) * 1.318);
-  var totalGeral = ((totalMastro + totalLona + totalbaseponteira + totalhora) * 1.318) * margemlucro;
+  var comp = ((totalMastro + totalLona + totalponteira + totalbase + totalhora) * 1.318);
+  var totalGeral = ((totalMastro + totalLona + totalponteira + totalbase + totalhora) * 1.318) * margemlucro;
 
   // Exibindo o resultado na página
   var resultado = document.getElementById("resultado");
   resultado.innerHTML = "<h2>Orçamento</h2>" +
     "<p>Total Mastro: R$" + totalMastro.toFixed(2) + "</p>" +
     "<p>Total Lona: R$" + totalLona.toFixed(2) + "</p>" +
-    "<p>Total Ponteiras: R$" + totalbaseponteira.toFixed(2) + "</p>" +
+    "<p>Total Ponteiras: R$" + totalponteira.toFixed(2) + "</p>" +
+    "<p>Total Bases: R$" + totalbase.toFixed(2) + "</p>" +
     "<p>Total Hora: R$" + totalhora.toFixed(2) + "</p>" +
     "<p>Composição: R$" + comp.toFixed(2) + "</p>" +
     "<p>Total Geral: R$" + totalGeral.toFixed(2) + "</p>";
