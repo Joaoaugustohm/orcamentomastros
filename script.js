e2dcc48685624a8371d4b5b25f33a7ca63ae6067
Joaoaugustohm <142924955+Joaoaugustohm@users.noreply.github.com>
2024-06-07 12:03:14 -0300

    comentarios

--- script.js
+++ script.js
@@ -21,18 +21,18 @@ function calcularOrcamento() {
   var homemhora = 40; // Preço por hora de mão de obra
 
   // Calculando o total para cada item
-  var quantidadeMastro = quantidadeMastro1 + quantidadeMastro2;
-  var tamanhoMastro = (tamanhoMastro1 * quantidadeMastro1) + (tamanhoMastro2 * quantidadeMastro2);
-  var totalMastro = (tamanhoMastro * precoMastro);
-  var totalponteira = (quantidadeMastro + quantidadeponteira) * precoPonteira;
-  var totalLona = metrosQuadrados * precoLona;
-  var totalbase = bases * precobase;
-  var totalhora = (metrosQuadrados * 0.8) * homemhora;
-  var margemlucro = (margemlucro / 100) + 1;
+  var quantidadeMastro = quantidadeMastro1 + quantidadeMastro2; //quantidade total de mastros 
+  var tamanhoMastro = (tamanhoMastro1 * quantidadeMastro1) + (tamanhoMastro2 * quantidadeMastro2); //tamanho total de mastros
+  var totalMastro = (tamanhoMastro * precoMastro); //total de mastros
+  var totalponteira = (quantidadeMastro + quantidadeponteira) * precoPonteira; //total de ponteira
+  var totalLona = metrosQuadrados * precoLona; //total de lona
+  var totalbase = bases * precobase; //total de bases
+  var totalhora = (metrosQuadrados * 0.8) * homemhora; //tempo de mão de obra
+  var margemlucro = (margemlucro / 100) + 1; //margem de lucro
 
   // Calculando o total geral
-  var comp = ((totalMastro + totalLona + totalponteira + totalbase + totalhora) * 1.318);
-  var totalGeral = ((totalMastro + totalLona + totalponteira + totalbase + totalhora) * 1.318) * margemlucro;
+  var comp = ((totalMastro + totalLona + totalponteira + totalbase + totalhora) * 1.318); //composição
+  var totalGeral = ((totalMastro + totalLona + totalponteira + totalbase + totalhora) * 1.318) * margemlucro; //preço final
 
   // Exibindo o resultado na página
   var resultado = document.getElementById("resultado");
